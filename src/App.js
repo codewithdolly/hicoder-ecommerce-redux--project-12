b70e16196876d22d49edaee853876c57f7feb9d1
hicoderindia <hicoder.dolly@gmail.com>
2022-02-08 23:36:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.scss";
 // https://fakestoreapi.com/products
-// import WebRoutes from "./WebRoutes";
+import WebRoutes from "./WebRoutes";
 
 function App() {
   return (
