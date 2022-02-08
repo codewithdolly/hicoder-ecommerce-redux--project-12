3c6007257b37bb66e0166858e40f9795f894f201
hicoderindia <hicoder.dolly@gmail.com>
2022-02-08 23:35:48 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
+// https://fakestoreapi.com/products
 import WebRoutes from "./WebRoutes";
 
 function App() {
   return (
     <>
       <div className="App">
+      Hi
         <WebRoutes />
       </div>
     </>
