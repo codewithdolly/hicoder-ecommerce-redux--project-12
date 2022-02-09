fc13a55532bd0788e6aaaa400a20cb11b61bf6d1
hicoderindia <hicoder.dolly@gmail.com>
2022-02-09 16:27:07 +0530

    ap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { Provider
  } from 'react-redux';
- import store from './Redux/store';
+import store from "./Redux/store"
 
 import App from './App';
 
@@ -16,6 +16,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
