1fec8f2bbc72f7d8f7984c262f4d557f1c0076b5
hicoderindia <hicoder.dolly@gmail.com>
2022-02-17 04:17:33 +0530

    API called

--- src/Components/ProductComponents/ProductComponents.js
+++ src/Components/ProductComponents/ProductComponents.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const ProductComponents = () => {
+  const products= useSelector((state) => state.allProducts.products);
+  const {id, title}= products[0];
+
   return (
     <>
         <div className='productComponents'>
-        <h2>Product Components</h2>
+        <div className="card" style={{width: "18rem"}}>
+  <img src="..." className="card-img-top" alt="..." />
+  <div className="card-body">
+  <h5 className="card-title">{title}</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
       </div>
     </>
   )
--- src/Components/ProductListing/ProductListing.js
+++ src/Components/ProductListing/ProductListing.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import ProductComponents from "../ProductComponents";
+import axios from "axios";
 
 const ProductListing = () => {
-  const products= useSelector((state) => state.allProducts.products);
-  console.log(products);
+  const products = useSelector((state) => state);
+  // console.log(products);
+
+  const productApi = `https://fakestoreapi.com/products`;
+  const fetchProducts = async () => {
+    const response = await axios.get(productApi).catch((error) => {
+      console.log(error);
+    });
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <>
-        <div className='productListing'><h2>Product Listing</h2></div>
+      <div className="productListing">
+        <h2>Product Listing</h2>
+      </div>
+      <ProductComponents />
     </>
-  )
-}
+  );
+};
 
-export default ProductListing
+export default ProductListing;
