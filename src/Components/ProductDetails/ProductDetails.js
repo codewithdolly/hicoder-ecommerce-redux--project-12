814fa0943716fa297582c0c5030deeea3c73c0a0
hicoderindia <hicoder.dolly@gmail.com>
2022-02-18 01:51:53 +0530

    Update ProductDetails.js

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -9,6 +9,7 @@ import {
 
 const ProductDetails = () => {
   const product = useSelector((state) => state.product);
+  const { image, title, price, category, description } = product;
 
   const { productId } = useParams();
   console.log(productId);
@@ -34,8 +35,40 @@ const ProductDetails = () => {
 
   return (
     <>
-      <div>
-        {Object.keys((product).length === 0 ? (<div>loading...</div>):(<div>Heyy</div>))}
+      <div className="container">
+        <div className="row my-5">
+          {Object.keys(product).length === 0 ? (
+            <div>loading...</div>
+          ) : (
+            <>
+              <div className="col-12 col-md-4 ">
+                {" "}
+                <img src={image} className=" shadow-sm p-4" width="500px" />
+              </div>
+              <div className="col-12 offset-md-2 col-md-6 mt-2 ">
+                <h3>{title}</h3>
+                <h2 className="text-">
+                  <a className="">${price}</a>
+                </h2>
+                
+                <button type="button" class="btn btn-light border btn-block mt-4 mb-2 text-capitalize">{category}</button>
+                <p>{description}</p>
+                <div className="">
+                  <div className="">
+                    <i className="shop icon"></i>
+                  </div>
+                 <div className="mt-3"> <button type="button" class="btn btn-success btn-lg mr-2 px-3">
+                    Buy Now
+                  </button>
+                  <button type="button" class="btn btn-warning btn-lg px-3">
+                    Add to Cart
+                  </button></div>
+                  
+                </div>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
