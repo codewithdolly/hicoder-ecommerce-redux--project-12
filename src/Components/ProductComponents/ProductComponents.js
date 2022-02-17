f1647d2d389ceb3c7d56078cfcb4f7391c2158c8
hicoderindia <hicoder.dolly@gmail.com>
2022-02-17 19:19:37 +0530

    Update ProductComponents.js

--- src/Components/ProductComponents/ProductComponents.js
+++ src/Components/ProductComponents/ProductComponents.js
@@ -5,39 +5,42 @@ import { useSelector } from "react-redux";
 
 const ProductComponents = () => {
   const products = useSelector((state) => state.allProducts.products);
-  // const { title, id, price, category, image } = products;
+
+
+  const renderList= products.map((product) => {
+    const { title, id, price, category, image } = product;
+    return (
+      <>
+        <div className="col-md-3 my-3" key={id}>
+          <Link to={`/product/${id}`} className="text-decoration-none text-dark">
+            <div className="card h-100 card-hover product-card ">
+              <img
+                src={image}
+                className="card-img-top img-thumbnail img-fluid shadow-sm p-4 border-bottom rounded-0"
+                alt={title}
+                style={{ height: "350px" }}
+              />
+              <div className="card-body p-2">
+                <p className="card-title">
+                  <b>{title}</b>
+                </p>
+                <p>
+                  <b>${price}</b>
+                  <p>{category}</p>
+                </p>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </>
+    );
+  })
 
   return (
     <>
       <div className="productComponents container-fluid">
         <div className="row mx-5 px-5 my-3">
-          {products.map((product) => {
-            return (
-              <>
-                <div className="col-md-3 my-3" key={product.id}>
-                  <Link to={`/product/${product.id}`}>
-                    <div className="card h-100 card-hover product-card ">
-                      <img
-                        src={product.image}
-                        className="card-img-top img-thumbnail img-fluid shadow-sm p-4 border-bottom rounded-0"
-                        alt={product.title}
-                        style={{ height: "350px" }}
-                      />
-                      <div className="card-body p-2">
-                        <p className="card-title">
-                          <b>{product.title}</b>
-                        </p>
-                        <p>
-                          <b>${product.price}</b>
-                          <p>{product.category}</p>
-                        </p>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              </>
-            );
-          })}
+         {renderList}
         </div>
       </div>
     </>
